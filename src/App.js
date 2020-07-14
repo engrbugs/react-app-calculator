0ab0cd10a659e6de88dd8c383015c4ce17a22c70
engrbugs <engrbugs@gmail.com>
2020-07-13 23:16:02 -0700

    2020-07-13-2315

--- src/App.js
+++ src/App.js
@@ -1,60 +1,62 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Converter from "./Converter";
+
+// function convertToWords (id) {
+//   return "one";
+// }
 
 const SVG = (props) => (
-  <svg width={props.width} height={props.height}
-  stroke={props.stroke} fill={props.fill}
-  viewBox="0 0 150 150" 
-  xmlns="http://www.w3.org/2000/svg">
-    <path d="M 0, 75
+  <svg
+    width="50px"
+    height="50px"
+    stroke={props.stroke}
+    fill={props.fill}
+    viewBox="0 0 150 150"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M 0, 75
       C 0, 18.75 18.75, 0 75, 0
       S 150, 18.75 150, 75
       131.25, 150 75, 150
-      0, 131.25 0, 75">
-      </path>
+      0, 131.25 0, 75"
+    ></path>
   </svg>
-)
+);
+
 const Numbers = (props) => (
-  <div class="button-center">
-    <svg width="50px" height="50px"
-    viewBox="0 0 150 150" 
-    stroke="#b2bec3" xmlns="http://www.w3.org/2000/svg">
-      <path d="M 0, 75
-          C 0, 18.75 18.75, 0 75, 0
-          S 150, 18.75 150, 75
-          131.25, 150 75, 150
-          0, 131.25 0, 75
-          " fill="#FAFAFA"></path>
-    </svg>
+  <div class="button-center" 
+  id={Converter(props.id)}>
+    <SVG stroke="#b2bec3" fill="#FAFAFA" />
     <a>{props.id}</a>
   </div>
-)
+);
 
-const Special = (props) => (
-  <div className={`button-special ${props.id === "Nan" && 'nan'}`}>
-    <svg width="50px" height="50px" 
-    viewBox="0 0 150 150" 
-    stroke="#fab1a0" xmlns="http://www.w3.org/2000/svg">
-      <path d="M 0, 75
-        C 0, 18.75 18.75, 0 75, 0
-        S 150, 18.75 150, 75
-        131.25, 150 75, 150
-        0, 131.25 0, 75
-        " fill="#d63031"></path>
-    </svg>
+function Special(props) {
+  return (
+    <div
+    className={`button-special ${props.id === "Nan" && "nan"}`}
+    id={Converter(props.id)}>
+    <SVG stroke="#fab1a0" fill="#d63031" />
     <a>{props.id}</a>
   </div>
-)
+);
+}
+  
 
-const Math = (props) => (
-  <div class="button-math">
-    <SVG width="50px" height="50px" 
-    stroke="#c8d6e5" fill="#FAFAFA" />
-    <a>{props.id}</a>
-  </div>
-)
+function Math(props) {
+  return (
+    <div class={props.id === '=' ? 'button-math-equals' : 'button-math'} 
+    id={Converter(props.id)}>
+      <SVG stroke={props.id === '=' ? '#c8d6e5' : '#c8d6e5'}
+      fill={props.id === '=' ? '#4185f4' : '#FAFAFA'} />
+      <a>{props.id}</a>
+    </div>
+  );
+}
 
 function App() {
   return (
@@ -65,19 +67,21 @@ function App() {
       <div id="display" className="display">
         72
       </div>
-		<div class="divider-2"><span></span> </div>
+      <div class="divider-2">
+        <span></span>{" "}
+      </div>
       <Special id="AC" />
       <Special id="Nan" />
       <Special id="Nan" />
-      <Math id="/" />
+      <Math id="÷" />
       <Numbers id="7" />
       <Numbers id="8" />
       <Numbers id="9" />
-      <Math id="*" />
+      <Math id="×" />
       <Numbers id="4" />
       <Numbers id="5" />
       <Numbers id="6" />
-      <Math id="-" />
+      <Math id="—" />
       <Numbers id="1" />
       <Numbers id="2" />
       <Numbers id="3" />
@@ -87,14 +91,6 @@ function App() {
       <Special id="Nan" />
       <Math id="=" />
     </div>
-
-
-
-
-
-
-    
-   
   );
 }
 
