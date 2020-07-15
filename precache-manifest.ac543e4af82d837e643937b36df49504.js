9933a57a9d57b8d97f61eede17bee4710dd973c5
engrbugs <engrbugs@gmail.com>
2020-07-15 12:25:00 -0700

    Updates

---
+++ precache-manifest.ac543e4af82d837e643937b36df49504.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15a886a0d49fc0573d2ce869a9dd03bb",
+    "url": "/react-app-calculator/index.html"
+  },
+  {
+    "revision": "7304500eae06eed302d9",
+    "url": "/react-app-calculator/static/css/main.c5c270a4.chunk.css"
+  },
+  {
+    "revision": "75a004e414a16ba54024",
+    "url": "/react-app-calculator/static/js/2.b5a824f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-calculator/static/js/2.b5a824f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7304500eae06eed302d9",
+    "url": "/react-app-calculator/static/js/main.d2ac0caa.chunk.js"
+  },
+  {
+    "revision": "51f6787e801f3600d57b",
+    "url": "/react-app-calculator/static/js/runtime-main.12fffe04.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
