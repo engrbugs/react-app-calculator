edc21929b1cddd76760b69cdf09e1cdb5c85192a
engrbugs <engrbugs@gmail.com>
2020-07-15 10:11:35 -0700

    2020-07-15-1011

--- src/App.js
+++ src/App.js
@@ -85,7 +85,6 @@ function App() {
       needReset = true;
       reset();
     }
-
     const value = e.target.innerText || e.target.id;
     switch (currentDisplay) {
       case "0":
@@ -121,13 +120,15 @@ function App() {
     //   needReset = true;
     //   reset();
     // }
-    let isNegative = currentDisplay.substr(0, 1) === "-" && true;
+    console.log('formula len:', formula.length);
+    let putZero = currentDisplay==='-' && true;
+    let isNegative = (currentDisplay.substr(0, 1) === "-") && true;
     if (!/LIMIT ERROR/.test(currentDisplay)) {
       formula === String.fromCharCode(160) && setFormula(""); // remove the empty char.
       const value = e.target.innerText ? e.target.innerText : e.target.id;
       switch (value) {
         case "÷":
-          !/\+|x|\//.test(currentDisplay) &&
+          !/\+|x|\/|—/.test(currentDisplay) &&
             setFormula(
               `${formula}${isNegative ? "(" : ""}${currentDisplay}${
                 isNegative ? ")" : ""
@@ -136,7 +137,7 @@ function App() {
           setCurrentDisplay("/");
           break;
         case "×":
-          !/\+|x|\//.test(currentDisplay) &&
+          !/\+|x|\/|—/.test(currentDisplay) &&
             setFormula(
               `${formula}${isNegative ? "(" : ""}${currentDisplay}${
                 isNegative ? ")" : ""
@@ -145,7 +146,7 @@ function App() {
           setCurrentDisplay("x");
           break;
         case "+":
-          !/\+|x|\//.test(currentDisplay) &&
+          !/\+|x|\/|—/.test(currentDisplay) &&
             setFormula(
               `${formula}${isNegative ? "(" : ""}${currentDisplay}${
                 isNegative ? ")" : ""
@@ -155,6 +156,7 @@ function App() {
           break;
         case "—":
           if (/\+|x|\/|—/.test(currentDisplay)) {
+            console.log('negative mode');
             // negative value
             if (currentDisplay==='x') {
               setFormula(formula + '*');
@@ -164,6 +166,14 @@ function App() {
               setFormula(formula + currentDisplay);
             }
             setCurrentDisplay("-");
+          } else if (isNegative) { // remove negativity
+            if (currentDisplay==='-') {
+              setCurrentDisplay('0');
+            } else {
+              setCurrentDisplay(currentDisplay.substr(1, currentDisplay.length));
+            };
+
+            
           } else {
             setFormula(formula + currentDisplay);
             setCurrentDisplay("—");
@@ -172,6 +182,7 @@ function App() {
         default:
       }
     }
+    console.log('formula len:', formula.length);
   }
 
   function maxLimitError() {
@@ -187,21 +198,23 @@ function App() {
   }
 
   function evaluate() {
+    console.log('formula:', formula, formula.length);
+    console.log('formula:', formula.trim(), formula.length); // len here is 3 only 2 lanf dapat
+    console.log('currentdisplau:', currentDisplay);
+    
     if (formula.substr(formula.length - 1) === "=") {
-    } else if (!/\+|x|\/|—|LIMIT ERROR/.test(currentDisplay)) {
+    } else if (!/\+|x|\/|—|-|LIMIT ERROR/.test(currentDisplay)) {
       let isNegative = currentDisplay.substr(0, 1) === "-" && true;
       let equation = `${formula}${isNegative ? "(" : ""}${currentDisplay}${
         isNegative ? ")" : ""
       }`
-      console.log(formula);
-      console.log(currentDisplay);
-      console.log(equation);
-      setCurrentDisplay(
-        eval(equation)
-      );
+      let answer = eval(equation);
+      console.log('equation:', equation);
+      console.log('answer:', answer);
+      setCurrentDisplay(answer);
       setFormula(equation + "=");
     } else if (!/LIMIT ERROR/.test(currentDisplay)) {
-      setCurrentDisplay(eval(formula));
+      setCurrentDisplay(eval(formula));// error here 9- * =
       setFormula(formula + "=");
     }
   }
